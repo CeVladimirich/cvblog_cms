0b4be88fccd50e295ba5d2c29f40a6e52b7699fb
CeVladimirich <55949668+CeVladimirich@users.noreply.github.com>
2022-10-29 15:44:18 +0000

    Update comments.php

--- admin/comments.php
+++ admin/comments.php
@@ -10,9 +10,7 @@ $elogin = $_SESSION['login'];
 $equery = mysqli_query($edblink, "SELECT * FROM ceadmins WHERE login = '$elogin'");
 $edata = mysqli_fetch_array($equery);
 
-//------------EDIT HERE------------
 $ep = intval($edata['status']);
-//---------------------------------
 
 mysqli_close($edblink);
 
@@ -20,7 +18,6 @@ $keyid = ;
 $keyid = md5($keyid);
 
 if ( ( (is_null($_SESSION['devid'])) || $ep != 1 ) || ($_SESSION['devid'] != $keyid) ) {
-	//echo $_SESSION['devid']." RECV ".$ep." MUST ".$keyid;
 	echo '<meta http-equiv="refresh" content="0;URL=?page=login">';
 
 	exit(0);
