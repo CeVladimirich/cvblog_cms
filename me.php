b7eac51d136bbc0e7b829ef82a52cd0e6e565a32
CeVladimirich <55949668+CeVladimirich@users.noreply.github.com>
2022-10-30 17:38:46 +0000

    Удалено за ненадобностью

--- me.php
+++
@@ -1,7 +0,0 @@
-
-<article class='post'>
-<div class='post-content'>
-<h2 class='post-title'>Измените файл <em>me.php</em> или удалите его.</h2>
-НЕ ЗАБУДЬТЕ УДАЛИТЬ ССЫЛКУ В ШАПКЕ, ЕСЛИ ВЫ УДАЛЯЕТЕ ME.PHP!
-</div>
-</article>
