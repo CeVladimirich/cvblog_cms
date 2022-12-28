1d5d74c490641ec81bba80404bde7adde710e668
CeVladimirich <me@cevladimirich.xyz>
2022-12-28 19:44:27 +0500

    Added data.sql, edited README.md and admin/settings.php

---
+++ admin/data.sql
@@ -0,0 +1,39 @@
+--
+-- By CeVladimirich
+-- CeBlog CMS, https://github.com/CeVladimirich/cvblog_cms
+--
+CREATE TABLE posts (
+    id int NOT NULL AUTO_INCREMENT,
+    date timestamp,
+    topicid int,
+    postflag int DEFAULT 2,
+    title text,
+    post text,
+    url text,
+    description text,
+    PRIMARY KEY(id)
+);
+CREATE TABLE topics (
+    id int NOT NULL AUTO_INCREMENT,
+    topic text, position int,
+    one_page bool,
+    PRIMARY KEY (id)
+);
+CREATE TABLE comments (
+    id int NOT NULL AUTO_INCREMENT,
+    date timestamp,
+    post_id int,
+    flag int default 1,
+    author text,
+    text text, PRIMARY KEY(id)
+);
+CREATE TABLE admins
+(
+    id       int NOT NULL AUTO_INCREMENT,
+    login    text,
+    password text,
+    status   int,
+    PRIMARY KEY (id)
+);
+INSERT INTO topics (topic, position) VALUES ('статьи', 0), ('заметки', 1), ('новости', 2), ('обо мне', 3);
+INSERT INTO admins (login, password, status) VALUES ('admin_cvblog', '21232f297a57a5a743894a0e4a801fc3', 1);
--- admin/settings.php
+++ admin/settings.php
@@ -31,58 +31,61 @@ mysqli_select_db($dblink, $dbname);
 mysqli_query($dblink, "SET NAMES 'utf8'");
 $tr = $_GET['mode'];
 switch($tr) {
+	case stts:
+		$sid = $_GET['id'];
+		$stts = $_GET['stts'];
+		mysqli_query($dblink, "UPDATE admins SET status = $stts WHERE id = $sid");
+		echo '<meta http-equiv="refresh" content="0;URL=?page=settings&mode=users">';
+		break;
+	case users:
+		echo '<b><a href="?page=settings&mode=adduser">ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ</a></b>';
+		echo '<center><table border="0" width="100%">';
+		echo '<tr><th>ЛОГИН</th><th>СТАТУС</th><th>ДЕЙСТВИЯ</th></tr>';
+		$query = mysqli_query($dblink, "SELECT * FROM admins ORDER BY id DESC");
+		while($data = mysqli_fetch_array($query)) {
+			echo '<tr><td>'.$data['login'].'</td><td>'.$data['status'].'</td><td>';
+			echo '<a href="?page=settings&mode=setpasswdfrm&id='.$data['id'].'">ИЗМЕНИТЬ ПАРОЛЬ</a><br>';
+			if ($_SESSION['login'] != $data['login']) {
+				if ($data['stts'] == '1') {
+					echo '<a href="?page=settings&mode=stts&stts=0&id=' . $data['id'] . '">ИЗМЕНИТЬ СТАТУС</a><br>';
+				}
+				if ($data['stts'] == '0') {
+					echo '<a href="?page=settings&mode=stts&stts=1&id=' . $data['id'] . '">ИЗМЕНИТЬ СТАТУС</a><br>';
+				}
+			}
+			echo '<a href="?page=settings&mode=edit&id='.$data['id'].'">ИЗМЕНИТЬ</a><br>';
+			echo '<hr></td></tr>';
+		}
+		echo '</table></center>';
+		break;
 case setpasswd:
-$tek_passwd = $_POST['tek_passwd'];
-$tek_passwd = md5($tek_passwd);
-$new = $_POST['new_passwd'];
-$re_new = $_POST['re_new_passwd'];
-$new = md5($new);
-$re_new = md5($re_new);
-$query = mysqli_query($dblink, "SELECT * FROM admins WHERE id = 1");
-$data = mysqli_fetch_array($query);
-$tek_mysql = $data['password'];
-if($tek_passwd == $tek_mysql and $new == $re_new) {
-	mysqli_query($dblink, "UPDATE admins SET password = '$new' WHERE id = 1");
-	echo '<meta http-equiv="refresh" content="0;URL=?page=settings&mode=site">';
-} else {
-echo '<b>ТЕКУЩИЙ ПАРОЛЬ ВВЕДЕН НЕПРАВИЛЬНО//ПАРОЛИ ВВЕДЕНЫ НЕПРАВИЛЬНО! Попробуйте еще...</b>';
-echo '<meta http-equiv="refresh" content="2;URL=?page=settings&mode=setpasswdfrm">';
-}
-break;
+	$sid = $_GET['id'];
+	$tek_passwd = $_POST['tek_passwd'];
+	$tek_passwd = md5($tek_passwd);
+	$new = $_POST['new_passwd'];
+	$re_new = $_POST['re_new_passwd'];
+	$new = md5($new);
+	$re_new = md5($re_new);
+	$query = mysqli_query($dblink, "SELECT * FROM admins WHERE id = $sid");
+	$data = mysqli_fetch_array($query);
+	$tek_mysql = $data['password'];
+	if($tek_passwd == $tek_mysql and $new == $re_new) {
+		mysqli_query($dblink, "UPDATE admins SET password = '$new' WHERE id = $sid");
+		echo '<meta http-equiv="refresh" content="0;URL=?page=settings&mode=users">';
+	} else {
+	echo '<b>ТЕКУЩИЙ ПАРОЛЬ ВВЕДЕН НЕПРАВИЛЬНО//ПАРОЛИ ВВЕДЕНЫ НЕПРАВИЛЬНО! Попробуйте еще...</b>';
+	echo '<meta http-equiv="refresh" content="2;URL=?page=settings&mode=setpasswdfrm&id='.$data['id'].'">';
+	}
+	break;
 case setpasswdfrm:
-echo '<center><form method="post" action="?page=settings&mode=setpasswd">';
-echo '<label>Текущий пароль: </label><input type="password" name="tek_passwd"><br><br>';
-echo '<label>Новый пароль: </label><input type="password" name="new_passwd"><br>';
-echo '<label>Повторите пароль: </label><input type="password" name="re_new_passwd"><br>';
-echo '<input type="submit" value="Применить">';
-echo '</form></center>';
-break;
-
-case setconf:
-include 'gen.php';
-$email = $_POST['email'];
-$name = $_POST['name'];
-$server1 = $server;
-$user1 = $user;
-$pw1 = $password;
-$dbname1 = $dbname;
-$url = $_POST['url'];
-$key = random_key();
-$data = '<?php
-$server = "'.$server1.'";
-$user = "'.$user1.'";
-$password = "'.$pw1.'";
-$dbname = "'.$dbname1.'";
-$email = "'.$email.'";
-$name = "'.$name.'";
-$url = "'.$url.'";
-$key = "'.$key.'";
-?>';
-$fo = fopen('config.php', 'w+');
-fwrite($fo, $data);
-fclose($fo);
-echo '<meta http-equiv="refresh" content="0;URL=?page=settings&mode=site">';
-break;
+	$id = $_GET['id'];
+	echo '<center><form method="post" action="?page=settings&mode=setpasswd&id='.$id.'">';
+	echo '<label>Текущий пароль: </label><input type="password" name="tek_passwd"><br><br>';
+	echo '<label>Новый пароль: </label><input type="password" name="new_passwd"><br>';
+	echo '<label>Повторите пароль: </label><input type="password" name="re_new_passwd"><br>';
+	echo '<input type="submit" value="Применить">';
+	echo '</form></center>';
+	break;
 case pos:
 $sid = $_GET['id'];
 $squery = mysqli_query($dblink, "SELECT * FROM topics WHERE id = $sid");
@@ -142,18 +145,6 @@ mysqli_query($dblink, "DELETE FROM topics WHERE id = $sid");
 mysqli_query($dblink, "UPDATE topics SET position = position - 1 WHERE position > $spos");
 echo '<meta http-equiv="refresh" content="0;URL=?page=settings&mode=topics">';
 break;
-case site:
-echo '<b>настройки сайта</b><br>';
-echo '<form method="post" action="?page=settings&mode=setconf">';
-echo '<label>Email: </label><input type="text" size="30" name="email" value="'.$email.'"><br>';
-echo '<label>Название сайта: </label><input type="text" size="30" name="name" value="'.$name.'"><br>';
-echo '<label>URL сайта: </label><input type="text" size="30" name="url" value="'.$url.'"><br>';
-echo '<input type="submit" value="Изменить"><br>';
-echo '</form>';
-echo '<em>ПЕРЕЗАГРУЗИТЕ СТРАНИЦУ ПОСЛЕ ПРИМЕНЕНИЯ И ЗАНОВО ВОЙДИТЕ В АДМИНКУ!</em><br>';
-echo '<b>админка</b><br>';
-echo '<b><a href="?page=settings&mode=setpasswdfrm">изменить пароль</a></b>';
-break;
 case topics:
 echo '<a href="?page=settings&mode=addtopic"><b>СОЗДАТЬ ТОПИК</b></a>';
 echo '<center><table border="0">';
@@ -172,6 +163,6 @@ echo '<a href="?page=settings&mode=del&id='.$data['id'].'">УДАЛИТЬ</a></t
 echo '</table></center>';
 break;
 default:
-echo '<center><b><a href="?page=settings&mode=topics">НАСТРОЙКИ РАЗДЕЛОВ</a> | <a href="?page=settings&mode=site">НАСТРОЙКИ САЙТА</a> | <a href="index.php">НА ГЛАВНУЮ</a></b></center>';
+echo '<center><b><a href="?page=settings&mode=topics">НАСТРОЙКИ РАЗДЕЛОВ</a> | <b><a href="?page=settings&mode=users">ПОЛЬЗОВАТЕЛИ</a></b> | <a href="index.php">НА ГЛАВНУЮ</a></b></center>';
 }
 ?>
