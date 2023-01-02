f5ce9264f7ba1d49f6704c6ba37218a853c6923e
CeVladimirich <volodimir.volkov.19@gmail.com>
2023-01-02 21:19:45 +0500

    фикс для работы индекса

--- inx.php
+++ inx.php
@@ -5,5 +5,5 @@ $topic = $_SESSION['indx_tpc'];
 $dblink = $db->start($server, $user, $password, $dbname);
 $query = $db->posts_query_index($dblink, $topic, "id", "DESC");
 $show = new db_show();
-return $show->show_posts_desc($query);
+return $show->show_posts_desc($query, $topic);
 ?>
