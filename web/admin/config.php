7d643f075c7c5b1d2a81f77c9992642537aff317
CeVladimirich <55949668+CeVladimirich@users.noreply.github.com>
2023-02-08 15:38:28 +0500

    delete all from config

--- web/admin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//======MySQL DATABASE======
-$server = "localhost"; // DB server, default - localhost
-$user = "cedbphp"; // DB user
-$password = "LbYe915#jh8TYn"; // DB password
-$dbname = "cetest"; // DB name
-
-$key = "KIKfrmkEAckeoc";
-?>
